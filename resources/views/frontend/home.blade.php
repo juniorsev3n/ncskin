be74afde7a707381c1414f75aadc3a11712bdb5e
irfan.maulana <irfan.maulana@wgs.co.id>
2018-01-06 00:32:47 +0700

    add slider

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,7 +5,41 @@
 
 <section class="section-home-banner">
 		    <div class="container">
-		      <img class="lazy" src="images/homepage-3-banner.jpg" />
+
+		    	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarousel" data-slide-to="1"></li>
+				    <li data-target="#myCarousel" data-slide-to="2"></li>
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner">
+				    <div class="item active">
+				      <img src="images/homepage-3-banner.jpg" alt="Los Angeles" class="lazy">
+				    </div>
+
+				    <div class="item">
+				      <img src="images/homepage-3-banner.jpg" alt="Chicago">
+				    </div>
+
+				    <div class="item">
+				      <img src="images/homepage-3-banner.jpg" alt="New York">
+				    </div>
+				  </div>
+
+				  <!-- Left and right controls -->
+				  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+		      <!--<img class="lazy" src="images/homepage-3-banner.jpg" />-->
 		    </div>
 		</section>
 
@@ -15,11 +49,11 @@
 		        <!-- Nav tabs -->
 		        <div class="tab-nav-holder">
 		            <ul class="nav-tabs">
-		                <li class="active">
+		                <li>
 		                    <a class="active-tab tab-control" href="#featured" data-toggle="featured">featured</a>
 		                </li>
 
-		                <li>
+		                <li class="active">
 		                    <a class="active-tab tab-control" href="#whats-new" data-toggle="whats-new">what's new</a>
 		                </li>
 		            </ul>
@@ -30,7 +64,7 @@
 		        </div>
 
 		        <div class="tab-content product-grid no-move-down">
-		            <div class="tab-pane active" id="featured">
+		            <div class="tab-pane" id="featured">
 		                <div class="row">
 						        @foreach ($products as $product)
                                 <div class="col-lg-4 col-xs-12 col-sm-6 product-holder">
@@ -79,7 +113,7 @@
 		                </div>
 		            </div><!-- /featured -->
 
-		            <div class="tab-pane" id="whats-new">
+		            <div class="tab-pane active" id="whats-new">
 		                <div class="row">
         <div class="col-lg-offset-0 col-lg-3 col-sm-4 col-sm-offset-1 col-xs-10 col-xs-offset-2 product-holder">
         </div>
